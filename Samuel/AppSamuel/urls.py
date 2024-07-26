ab7e5021e900ceab07063608ed5a18ae617475bf
Samuel M <samuelmaydana1@gmail.com>
2024-07-26 11:05:42 -0300

    Formulario con html y con django

--- Samuel/AppSamuel/urls.py
+++ Samuel/AppSamuel/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('cursos/', views.cursos, name='Cursos'),
     path('estudiantes/', views.estudiantes, name='Estudiantes'),
     path('profesores/', views.profesores, name='Profesores'),
-    path('entregables/', views.entregables, name='Entregables')
+    path('entregables/', views.entregables, name='Entregables'),
+    path('curso-formulario/', views.curso_formulario, name="CursoFormulario"),
+    path('form-con-api/', views.form_con_api, name="FormConApi")
 ]
--- Samuel/AppSamuel/views.py
+++ Samuel/AppSamuel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from AppSamuel.models import Curso
+from AppSamuel.forms import CursoFormulario
 
 def inicio(request):
     return render(request, 'AppSamuel/index.html')
@@ -15,3 +16,33 @@ def profesores(request):
 
 def entregables(request):
     return render(request, 'AppSamuel/entregables.html')
+
+def curso_formulario(request):
+
+    if request.method == 'POST':
+
+        curso = Curso(
+            nombre=request.POST['curso'],
+            camada=request.POST['camada']
+        )
+        curso.save()
+
+        return render(request, "AppSamuel/index.html")
+
+    return render(request,"AppSamuel/curso_formulario.html")
+
+def form_con_api(request):
+    if request.method == "POST":
+        mi_formulario = CursoFormulario(request.POST) # Aqui me llega la informacion del html
+        # print(miFormulario)
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+            
+            curso = Curso(nombre=informacion["curso"], camada=informacion["camada"])
+            curso.save()
+
+            return render(request, "AppSamuel/index.html")
+    else:
+        mi_formulario = CursoFormulario()
+
+    return render(request, "AppSamuel/form_con_api.html", {"mi_formulario": mi_formulario})
